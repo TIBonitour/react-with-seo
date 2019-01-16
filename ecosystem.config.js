d07aa0542fa338bfd300accdc9ddb3a2975651dd
Fabiano Correia <fabinppk@hotmail.com>
2019-01-16 17:52:50 -0200

    npm install with sudo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:TIBonitour/react-with-seo.git',
       path : '/home/ubuntu/react-with-seo',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'sudo npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
 
